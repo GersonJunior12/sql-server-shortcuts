f1cacc3210283c839565e320fd12c9acc2aa5112
GersonJunior12 <gerson_junior_12@hotmail.com>
2025-05-18 23:46:00 -0300

    Ajusts

--- Ctrl + 9/plain-script.sql
+++ Ctrl + 9/plain-script.sql
@@ -1,7 +1,7 @@
 DECLARE @TableName NVARCHAR(100) = 'test'
 
 SELECT 
-	STRING_AGG(c.name, ', ') AS Colunas 
+	STRING_AGG(c.name, ', ') AS Columns 
 FROM sys.tables t 
 INNER JOIN sys.columns c ON t.object_id = c.object_id 
 WHERE t.name = @TableName
--- Ctrl + 9/shortcut-version.sql
+++ Ctrl + 9/shortcut-version.sql
@@ -1,1 +1,1 @@
-sp_executesql N'SELECT 	STRING_AGG(c.name, '', '') AS Colunas FROM sys.tables t INNER JOIN sys.columns c ON t.object_id = c.object_id WHERE t.name = @TableName', N'@TableName nvarchar(776)', @TableName =
+sp_executesql N'SELECT 	STRING_AGG(c.name, '', '') AS Columns FROM sys.tables t INNER JOIN sys.columns c ON t.object_id = c.object_id WHERE t.name = @TableName', N'@TableName nvarchar(776)', @TableName =
